1a67b8ed78d257c969df9ed3b84b04d5d02a4b00
LiulietLee <lee.liuliet@hotmail.com>
2017-06-21 21:09:32 +0800

    update

--- BCD/ViewController.swift
+++ BCD/ViewController.swift
@@ -8,14 +8,29 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
 
     @IBOutlet weak var textField: UITextField!
+    @IBOutlet weak var goButton: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        goButton.isEnabled = false
+        textField.delegate = self
         textField.becomeFirstResponder()
     }
-
+    
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        if textField.text == "" && string == "0" {
+            return false
+        }
+        
+        if textField.text?.characters.count == 1 && string == "" {
+            goButton.isEnabled = false
+        } else {
+            goButton.isEnabled = true
+        }
+        return true
+    }
 }
 
